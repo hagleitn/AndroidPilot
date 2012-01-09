bf99091c3b34015be23fc4dcd50b8c866fb99fba
barbermot <hagleitn@172.16.0.26>
2012-01-08 22:45:29 -0800

    forgot to checkin file

--- src/com/barbermot/pilot/rc/NetworkRemote.java
+++ src/com/barbermot/pilot/rc/NetworkRemote.java
@@ -2,103 +2,45 @@ package com.barbermot.pilot.rc;
 
 import ioio.lib.api.exception.ConnectionLostException;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeoutException;
 
-import org.apache.log4j.Logger;
-
-import com.barbermot.pilot.flight.FlightConfiguration;
 import com.barbermot.pilot.quad.QuadCopter;
 import com.barbermot.pilot.quad.QuadCopter.Direction;
 
-public class NetworkRemote implements Runnable {
+public class NetworkRemote extends RemoteControl {
     
-    QuadCopter      ufo;
-    ServerSocket    server;
-    ProtocolHandler handler;
-    ExecutorService executor;
-    boolean[]       controlMap = { true, true, true, true };            ;
-    Logger          logger     = Logger.getLogger(NetworkRemote.class);
+    SwitchedQuadCopter manualControlUfo;
+    SwitchedQuadCopter computerControlUfo;
     
-    public NetworkRemote() throws IOException {
-        server = new ServerSocket(FlightConfiguration.get()
-                .getRemoteControlPort());
+    public NetworkRemote(QuadCopter ufo, SwitchedQuadCopter manual,
+            SwitchedQuadCopter computer) {
+        super(ufo);
+        manualControlUfo = manual;
+        computerControlUfo = computer;
     }
     
-    public void setExecutorService(ExecutorService executor) {
-        this.executor = executor;
-    }
-    
-    public void setUfo(QuadCopter ufo) {
-        this.ufo = ufo;
-    }
-    
-    public void setEnabled(Direction d, boolean enable) {
-        controlMap[d.ordinal()] = enable;
-    }
-    
-    private class ProtocolHandler implements Runnable {
+    @Override
+    public void setControlMask(char mask) throws ConnectionLostException {
+        super.setControlMask(mask);
         
-        Socket                  socket;
-        InputStream             in;
-        public final static int CHANNELS = 4;
-        Logger                  logger   = Logger.getLogger(ProtocolHandler.class);
+        mask = 0x1;
         
-        public ProtocolHandler(Socket socket) throws IOException {
-            this.socket = socket;
-            this.socket.shutdownOutput();
-            this.in = socket.getInputStream();
-        }
-        
-        private void parse(byte[] buffer) throws ConnectionLostException {
-            Direction[] d = Direction.values();
-            for (int i = 0; i < CHANNELS; ++i) {
-                if (controlMap[i]) {
-                    ufo.move(d[i], (int) buffer[i]);
-                }
+        for (Direction d : Direction.values()) {
+            if ((controlMask & mask) != 0) {
+                manualControlUfo.enable(d);
+                computerControlUfo.disable(d);
+            } else {
+                manualControlUfo.disable(d);
+                computerControlUfo.enable(d);
             }
-        }
-        
-        @Override
-        public void run() {
-            byte[] buffer = new byte[CHANNELS];
-            try {
-                int count = 0;
-                while (!Thread.interrupted()) {
-                    count += in.read(buffer, count, CHANNELS - count);
-                    if (count == CHANNELS) {
-                        count = 0;
-                        parse(buffer);
-                    }
-                }
-            } catch (IOException e) {
-                logger.info("Lost Connection.", e);
-            } catch (ConnectionLostException e) {
-                logger.info("Lost IOIO.", e);
-            } finally {
-                try {
-                    if (socket != null) {
-                        socket.shutdownInput();
-                        socket.close();
-                    }
-                } catch (IOException e) {
-                    // don't care.
-                }
-            }
+            mask = (char) (mask << 1);
         }
     }
     
     @Override
-    public void run() {
-        while (!Thread.interrupted()) {
-            try {
-                executor.submit(new ProtocolHandler(server.accept()));
-            } catch (IOException e) {
-                logger.warn("No connection", e);
-            }
-        }
+    protected int readManualThrottle() throws ConnectionLostException,
+            TimeoutException {
+        return manualControlUfo.readRaw(Direction.VERTICAL);
     }
+    
 }
